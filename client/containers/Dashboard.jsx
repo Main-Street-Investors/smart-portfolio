ada9847301ae1b9a5828b2e23f6cad9a5bf07ec2
Travis Frank <travis@travismfrank.com>
2020-09-30 16:54:51 -0400

    Implemented Login functionality on frontend

--- client/containers/Dashboard.jsx
+++ client/containers/Dashboard.jsx
@@ -11,7 +11,7 @@ class Dashboard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      loading: false,
+      loading: true,
       portfolioNum: 1,
       data: [
         { x: 1, y: 2 },
@@ -21,6 +21,15 @@ class Dashboard extends Component {
         { x: 5, y: 7 }
       ]
     };
+  }
+
+  componentDidMount() {
+    fetch('/api/getPortfolio')
+    .then(resp => resp.json())
+    .then(this.setState({
+      ...this.state,
+      loading: false
+    }))
   }
 
   render() {
--- client/containers/Login.jsx
+++ client/containers/Login.jsx
@@ -4,21 +4,49 @@ import Button from 'react-bootstrap/Button';
 class Login extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      usernameInput: '',
+      passwordInput: ''
+    }
   }
 
   render() {
     return(
       <div>
-      <h1 className="logo">Smart Portfolio</h1>
-      <div>
-      <h3 className="headline">Log in</h3>
-      <div>
-      Username: <input className="usernameinput" ></input>
-      <div>
-      Password: <input className="passwordinput" type="password"></input>
-      <div>
-      <Button variant="outline-info">Submit</Button>
-                   </div>
+        <h1 className="logo">Smart Portfolio</h1>
+          <div>
+            <h3 className="headline">Log in</h3>
+            <div>
+              Username: <input className="usernameinput" onChange={(e) => {
+                this.setState({
+                  ...this.state,
+                  usernameInput: e.target.value
+                });
+              }}></input>
+              <div>
+                Password: <input className="passwordinput" type="password" onChange={(e) => {
+                  this.setState({
+                    ...this.state,
+                    passwordInput: e.target.value
+                  });
+                }}></input>
+                <div>
+                  <Button variant="outline-info" onClick={() => {
+                    fetch('/api/regularLogin', {
+                      method: 'POST',
+                      body: JSON.stringify({
+                        "username": this.state.usernameInput,
+                        "password": this.state.passwordInput,
+                      }),
+                      headers: {
+                        'Content-Type': 'application/json'
+                      }
+                    })
+                    .then(resp => {
+                      window.location.href = resp.url;
+                    })
+                  }}>Submit</Button>
+                </div>
               </div>
            </div>
         </div>
