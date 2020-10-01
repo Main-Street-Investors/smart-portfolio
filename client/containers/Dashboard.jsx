7c056ad1ff48b143e8a34c8fe9fe66cb329ef649
Travis Frank <travis@travismfrank.com>
2020-09-30 23:35:52 -0400

    Portfolio + Dashboard data display updates

--- client/containers/Dashboard.jsx
+++ client/containers/Dashboard.jsx
@@ -5,6 +5,8 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Spinner from 'react-bootstrap/Spinner';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
 import { Link } from 'react-router-dom';
 
 
@@ -14,75 +16,138 @@ class Dashboard extends Component {
     this.state = {
       loading: true,
       portfolioNum: 0,
+      portfolioName: 'Consolidated Portfolio',
       range: 90,
-      data: [],
+      data: {},
       portfolioChartButtons: [],
-      totalValue: 13654.88,
-      acquiredValue: 12362.01,
-      numberOfShares: 156,
+      totalValue: 0,
+      acquiredValue: 0,
+      numberOfShares: 0,
+      showIndividualShares: false,
+      tickerNames: [],
+      ticker: ''
     };
+    this.ticker = '';
+  }
+
+  // Function to update state for Dashboard, used with either local storage cache or fresh request
+  populateStateWithStockData(data) {
+    console.log(data);
+    data.IEXData.map(cv => {
+      cv.data = cv.data.map(point => {
+        point.x = new Date(point.x);
+        point.x.setHours(0);
+        return point;
+      })
+      return cv;
+    });
+    data.chartData.map(cv => {
+      cv.data = cv.data.map(point => {
+        point.x = new Date(point.x);
+        point.x.setHours(0);
+        return point;
+      })
+      return cv;
+    });
+    const buttons = data.chartData.reduce((acc, cv, i) => {
+      // Skip the first, since it's the consolidated data
+      if (i === 0) return acc;
+
+      // Create a button that will reset state to show data for this portfolio
+      acc.push(<Button className="dashNavBtn" variant="outline-info" key={`portfolioBtn${i}`} block onClick={() => {
+        const portName = this.state.data.chartData[i].portfolioName;
+        const totalValue = this.state.data.chartData[i].data[63].y;
+        const acquiredValue = this.state.data.currentShares.reduce((acc, cv) => {
+          if (cv.name === portName) {
+            acc += cv.price * cv.number_shares;
+          };
+          return acc;
+        }, 0);
+        const numberOfShares = this.state.data.currentShares.reduce((acc, cv) => {
+          if (cv.name === portName) {
+            acc += cv.number_shares;
+          };
+          return acc;
+        }, 0) - this.state.data.soldShares.reduce((acc, cv) => {
+          if (cv.name === portName) {
+            acc += cv.number_shares;
+          };
+          return acc;
+        }, 0);
+        this.setState({
+          ...this.state,
+          portfolioNum: i,
+          portfolioName: portName,
+          totalValue,
+          acquiredValue,
+          numberOfShares
+        });
+      }}>{cv.portfolioName}</Button>);
+      return acc;
+    }, []);
+
+    // Calculate values for glance
+    const totalValue = data.chartData[0].data[63].y;
+    const acquiredValue = data.currentShares.reduce((acc, cv) => {
+      acc += cv.price * cv.number_shares;
+      return acc;
+    }, 0);
+    const numberOfShares = data.currentShares.reduce((acc, cv) => {
+      acc += cv.number_shares;
+      return acc;
+    }, 0) - data.soldShares.reduce((acc, cv) => {
+      acc += cv.number_shares;
+      return acc;
+    }, 0);
+
+    // Create options for individual stocks modal
+    const tickerNames = data.IEXData.map(cv => {
+      return <option key={`ticker-${cv.ticker}`}>{cv.ticker}</option>
+    });
+
+    // Store data in local storage
+    if (!data.accessed) {
+      data.accessed = new Date();
+      window.localStorage.setItem('spData', JSON.stringify(data));
+    }
+
+    // Finally, update state and set loading to false
+    this.setState({
+      ...this.state,
+      loading: false,
+      data,
+      portfolioChartButtons: buttons,
+      totalValue,
+      acquiredValue,
+      numberOfShares,
+      tickerNames
+    });
   }
 
   componentDidMount() {
-    fetch('/api/getPortfolio')
-    .then(resp => resp.json())
-    .then(data => {
-      console.log(data);
-      data.IEXData.map(cv => {
-        cv.data = cv.data.map(point => {
-          point.x = new Date(point.x);
-          point.x.setHours(0);
-          return point;
-        })
-        return cv;
+    const spData = window.localStorage.getItem('spData');
+    if (spData) {
+      let data = JSON.parse(spData);
+      if (Date.now() - new Date(data.accessed).getTime() > 86400000) {
+        // Refresh the data
+        window.localStorage.removeItem('spData');
+        fetch('/api/getPortfolio')
+        .then(resp => resp.json())
+        .then(data => {
+          this.populateStateWithStockData(data);
+        });
+      } else {
+        this.populateStateWithStockData(data);
+      }
+    } else {
+      // Don't have the data, go get it
+      fetch('/api/getPortfolio')
+      .then(resp => resp.json())
+      .then(data => {
+        this.populateStateWithStockData(data);
       });
-      data.chartData.map(cv => {
-        cv.data = cv.data.map(point => {
-          point.x = new Date(point.x);
-          point.x.setHours(0);
-          return point;
-        })
-        return cv;
-      });
-      const buttons = data.chartData.reduce((acc, cv, i) => {
-        if (i === 0) return acc;
-        acc.push(<Button className="dashNavBtn" variant="outline-info" key={`portfolioBtn${i}`} block onClick={() => {
-          this.setState({
-            ...this.state,
-            portfolioNum: i
-          });
-        }}>{cv.category}</Button>);
-        return acc;
-      }, []);
-      const totalValue = data.currentShares.reduce((acc, cv) => {
-        acc += cv.price * cv.number_shares;
-        return acc;
-      }, 0) - data.soldShares.reduce((acc, cv) => {
-        acc += cv.sell_price * cv.number_shares;
-        return acc;
-      }, 0);
-      const acquiredValue = data.currentShares.reduce((acc, cv) => {
-        acc += cv.price * cv.number_shares;
-        return acc;
-      }, 0);
-      const numberOfShares = data.currentShares.reduce((acc, cv) => {
-        acc += cv.number_shares;
-        return acc;
-      }, 0) - data.soldShares.reduce((acc, cv) => {
-        acc += cv.number_shares;
-        return acc;
-      }, 0);
-      this.setState({
-        ...this.state,
-        loading: false,
-        data: data.chartData,
-        portfolioChartButtons: buttons,
-        totalValue,
-        acquiredValue,
-        numberOfShares
-      })
-    })
-  }
+    };
+  };
 
   render() {
     return(
@@ -98,19 +163,40 @@ class Dashboard extends Component {
           <Row style={{height: '90vh'}}>
             <Col lg={2} id="nav">
               <div>
-                <Link to="/portfolio/1">
+                <Link to="/portfolio">
                   <Button variant="outline-info" block>
                     Manage Your Portfolios
                   </Button>
                 </Link>
                 <Button className="dashNavBtn" variant="outline-info" block onClick={() => {
+                  const totalValue = this.state.data.chartData[0].data[63].y;
+                  const acquiredValue = this.state.data.currentShares.reduce((acc, cv) => {
+                    acc += cv.price * cv.number_shares;
+                    return acc;
+                  }, 0);
+                  const numberOfShares = this.state.data.currentShares.reduce((acc, cv) => {
+                    acc += cv.number_shares;
+                    return acc;
+                  }, 0) - this.state.data.soldShares.reduce((acc, cv) => {
+                    acc += cv.number_shares;
+                    return acc;
+                  }, 0);
                   this.setState({
                     ...this.state,
-                    portfolioNum: 0
+                    portfolioNum: 0,
+                    portfolioName: 'Consolidated Portfolio',
+                    totalValue,
+                    acquiredValue,
+                    numberOfShares
                   });
                 }}>Consolidated Portfolios</Button>
                 {this.state.portfolioChartButtons}
-                <Button variant="outline-info" block>Individual Stocks</Button>
+                <Button variant="outline-info" block onClick={() => {
+                  this.setState({
+                    ...this.state,
+                    showIndividualShares: true
+                  });
+                }}>Individual Stocks</Button>
               </div>
               <div id="logoutDiv">
                 <Link to="/">
@@ -125,7 +211,7 @@ class Dashboard extends Component {
               <Row style={{height: '90vh'}}>
                 <Col lg={10}>
                   <Row className="centerColumn">
-                    <h3 id="chartTitle">{this.state.portfolioNum === 0 ? 'Consolidated Portfolio Metrics' : `${this.state.data[this.state.portfolioNum].category} Metrics`}</h3>
+                    <h3 id="chartTitle">{this.state.portfolioNum < 0 ? `${this.state.ticker} Metrics` : this.state.portfolioNum === 0 ? 'Consolidated Portfolio Metrics' : `${this.state.data.chartData[this.state.portfolioNum].portfolioName} Metrics`}</h3>
                   </Row>
                   <Row className="centerColumn">
                     <div id="chartContainer">
@@ -142,9 +228,27 @@ class Dashboard extends Component {
                             data: { stroke: "#b81752" },
                             parent: { border: "1px solid #b81752"}
                           }}
-                          data={this.state.range === 90 ? this.state.data[this.state.portfolioNum].data :
-                            this.state.range === 30 ? this.state.data[this.state.portfolioNum].data.slice(this.state.data[this.state.portfolioNum].data.length - 29) :
-                            this.state.data[this.state.portfolioNum].data.slice(this.state.data[this.state.portfolioNum].data.length - 3)
+                          data={this.state.portfolioNum < 0 && this.state.range === 90 ? this.state.data.IEXData.reduce((acc, cv) => {
+                              if (cv.ticker === this.state.ticker) {
+                                acc = cv.data;
+                              }
+                              return acc;
+                            }, []) :
+                            this.state.portfolioNum < 0 && this.state.range === 30 ? this.state.data.IEXData.reduce((acc, cv) => {
+                                if (cv.ticker === this.state.ticker) {
+                                  acc = cv.data;
+                                }
+                                return acc;
+                              }, []).slice(35) :
+                              this.state.portfolioNum < 0 ? this.state.data.IEXData.reduce((acc, cv) => {
+                                  if (cv.ticker === this.state.ticker) {
+                                    acc = cv.data;
+                                  }
+                                  return acc;
+                                }, []).slice(61) :
+                            this.state.range === 90 ? this.state.data.chartData[this.state.portfolioNum].data :
+                            this.state.range === 30 ? this.state.data.chartData[this.state.portfolioNum].data.slice(35) :
+                            this.state.data.chartData[this.state.portfolioNum].data.slice(61)
                           }
                         />
                       </VictoryChart>
@@ -173,7 +277,7 @@ class Dashboard extends Component {
                 </Col>
                 <Col lg={2}>
                   <div id="glance">
-                    <h4>Portfolio {this.state.portfolioNum}</h4>
+                    <h4>{this.state.portfolioName}</h4>
                     <p><i>at a Glance</i></p>
                     <p>- - - - - - -</p>
                     <h5>Total Value</h5>
@@ -190,7 +294,7 @@ class Dashboard extends Component {
                     </div>
                     <h5>Profit</h5>
                     <div className="glanceInfo">
-                      <p className="glanceInfoText">{`${Math.round(this.state.acquiredValue / this.state.totalValue * 10 * 100) / 100}%`}</p>
+                      <p className="glanceInfoText">{`${this.state.totalValue - this.state.acquiredValue < 0 ? '-' : ''}${this.state.totalValue - this.state.acquiredValue < 0 ? Math.round((1 - (this.state.totalValue / this.state.acquiredValue)) * 10000) / 100 : Math.round((1 - (this.state.acquiredValue / this.state.totalValue)) * 10000) / 100}%`}</p>
                     </div>
                     <h5>Number of Shares</h5>
                     <div className="glanceInfo">
@@ -202,6 +306,39 @@ class Dashboard extends Component {
             </Col>
           </Row>
         </Container>}
+        <Modal centered show={this.state.showIndividualShares} onHide={() => {
+          this.setState({
+            ...this.state,
+            showIndividualShares: false
+          });
+        }}>
+          <Modal.Header closeButton>
+            <Modal.Title>Individual Stock Performance</Modal.Title>
+          </Modal.Header>
+          <Form>
+            <Form.Group>
+              <Form.Label>Select a Stock:</Form.Label>
+              <Form.Control as="select" onChange={e => {
+                this.ticker = e.target.value;
+              }}>
+                <option>Select a Stock</option>
+                {this.state.tickerNames}
+              </Form.Control>
+            </Form.Group>
+          </Form>
+          <Modal.Footer>
+            <Button variant="outline-info" onClick={(e) => {
+              this.setState({
+                ...this.state,
+                showIndividualShares: false,
+                portfolioNum: this.ticker === 'Select a Stock' ? this.state.portfolioNum : -1,
+                ticker: this.ticker === 'Select a Stock' ? this.state.ticker : this.ticker
+              });
+            }}>
+              Submit
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </div>
     )
   }
--- client/containers/Portfolio.jsx
+++ client/containers/Portfolio.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import {
   Switch,
@@ -20,7 +20,6 @@ function Portfolio() {
   let match = useRouteMatch();
   let location = useLocation();
 
-
   const [historical, setHistorical] = useState(false);
 
   const [showNewPortfolio, setNewPortfolio] = useState(false);
@@ -28,7 +27,44 @@ function Portfolio() {
 
   if (!historical && location.pathname.match(/history/g)) setHistorical(true);
 
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  const data = JSON.parse(window.localStorage.getItem('spData'));
+
+  const buttons = data.chartData.reduce((acc, cv, i) => {
+    // Skip the first, since it's the consolidated data
+    if (i === 0) return acc;
+
+    let portfolioID = 0;
+
+    for (let i = 0; i < data.currentShares.length; i++) {
+      if (data.currentShares[i].name === cv.portfolioName) {
+        portfolioID = data.currentShares[i].portfolio_id;
+        break;
+      }
+    }
+
+    // Create a button that links to a different management chart
+    acc.push(<Link key={`portfolioBtn${i}`} to={`/Portfolio/${portfolioID}`}>
+      <Button className={acc.length === 0 ? '' : 'manageNav'} variant="outline-info" block onClick={() => {
+        setHistorical(false);
+      }}>{cv.portfolioName}</Button>
+    </Link>);
+    return acc;
+  }, []);
+
+  useEffect(() => {
+    console.log(data);
+
+    if (window.location.pathname === match.path) {
+      window.location.href = `${match.url}/${data.currentShares[1].portfolio_id}`;
+    }
+
+    setLoading(false);
+  }, [])
+
+
+
 
   return (
     <div>
@@ -37,62 +73,61 @@ function Portfolio() {
           <span className="sr-only">Loading...</span>
         </Spinner>
       </div>}
-    {!loading && <div>
-      <div className="topPad"></div>
-      <Container fluid={true}>
-        <Row style={{height: '90vh'}}>
-          <Col lg={2} id="nav">
-            <div>
-              <Button variant="outline-info" block>Portfolio 1</Button>
-              <Button variant="outline-info" block>Portfolio 2</Button>
-              <Button variant="outline-info" block onClick={() => {
-              setNewPortfolio(true);}}>Create New Portfolio</Button>
-            </div>
-            <div id="logoutDiv">
-              {historical && <Link to={`/Portfolio/${location.pathname.slice(11, 12)}`}>
-                <Button variant="outline-info" block onClick={() => {
-                  setHistorical(false);
-                }}>Back to Manager</Button>
-              </Link>}
-              <Link to="/Dashboard">
-                <Button id="backToDash" variant="outline-info" block>Back to Dashboard</Button>
-              </Link>
-              <Link to="/">
-                <Button id="logoutBtn" variant="outline-secondary" block>
-                  Logout
-                </Button>
-              </Link>
-            </div>
-          </Col>
-          <Col>
-            <Switch>
-              <Route path={`${match.path}/:portfolioID`} exact>
-                <PortfolioManager setHistorical={setHistorical}/>
-              </Route>
-              <Route path={`${match.path}/:portfolioID/history`}>
-                <PortfolioHistory />
-              </Route>
-            </Switch>
-          </Col>
-        </Row>
-      </Container>
-      <Modal centered show={showNewPortfolio} onHide={handleNewPortfolioClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>New Portfolio</Modal.Title>
-        </Modal.Header>
-        <Form>
-          <Form.Group>
-            <Form.Label>Name of Portfolio:</Form.Label>
-            <Form.Control type="text" placeholder="Portfolio"/>
-          </Form.Group>
-        </Form>
-        <Modal.Footer>
-          <Button variant="outline-info" onClick={handleNewPortfolioClose}>
-            Create Portfolio
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </div>}
+      {!loading && <div>
+        <div className="topPad"></div>
+        <Container fluid={true}>
+          <Row style={{height: '90vh'}}>
+            <Col lg={2} id="nav">
+              <div>
+                {buttons}
+                <Button className="manageNav" variant="outline-info" block onClick={() => {
+                setNewPortfolio(true);}}>Create New Portfolio</Button>
+              </div>
+              <div id="logoutDiv">
+                {historical && <Link to={`/Portfolio/${location.pathname.slice(11, 12)}`}>
+                  <Button variant="outline-info" block onClick={() => {
+                    setHistorical(false);
+                  }}>Back to Manager</Button>
+                </Link>}
+                <Link to="/Dashboard">
+                  <Button id="backToDash" variant="outline-info" block>Back to Dashboard</Button>
+                </Link>
+                <Link to="/">
+                  <Button id="logoutBtn" variant="outline-secondary" block>
+                    Logout
+                  </Button>
+                </Link>
+              </div>
+            </Col>
+            <Col>
+              <Switch>
+                <Route path={`${match.path}/:portfolioID`} exact>
+                  <PortfolioManager setHistorical={setHistorical} data={data}/>
+                </Route>
+                <Route path={`${match.path}/:portfolioID/history`}>
+                  <PortfolioHistory data={data}/>
+                </Route>
+              </Switch>
+            </Col>
+          </Row>
+        </Container>
+        <Modal centered show={showNewPortfolio} onHide={handleNewPortfolioClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>New Portfolio</Modal.Title>
+          </Modal.Header>
+          <Form>
+            <Form.Group>
+              <Form.Label>Name of Portfolio:</Form.Label>
+              <Form.Control type="text" placeholder="Portfolio"/>
+            </Form.Group>
+          </Form>
+          <Modal.Footer>
+            <Button variant="outline-info" onClick={handleNewPortfolioClose}>
+              Create Portfolio
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>}
     </div>
   );
 }
@@ -100,6 +135,14 @@ function Portfolio() {
 function PortfolioManager(props) {
   let { portfolioID } = useParams();
   const [portID, setPortID] = useState(portfolioID);
+  const [portName, setPortName] = useState('');
+
+  const [currentShareID, setCurrentShareID] = useState(null);
+  const [selectedTicker, setSelectedTicker] = useState(null);
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [selectedPrice, setSelectedPrice] = useState(null);
+  const [selectedNumberShares, setSelectedNumberShares] = useState(null);
+  const [selectedTags, setSelectedTags] = useState(null);
 
   const [showRenamePortfolio, setRenamePortfolio] = useState(false);
   const [showNewStock, setNewStock] = useState(false);
@@ -111,9 +154,65 @@ function PortfolioManager(props) {
   const handleSellStockClose = () => setSellStock(false);
   const handleEditStockClose = () => setEditStock(false);
 
+  const onInput = e => {
+    setCurrentShareID(e.target.value);
+  }
+
+  useEffect(() => {
+    for (let i = 0; i < props.data.currentShares.length; i++) {
+      if (props.data.currentShares[i]._id === Number(currentShareID)) {
+        setSelectedTicker(props.data.currentShares[i].ticker_name);
+        setSelectedDate(props.data.currentShares[i].date_purchased);
+        setSelectedPrice(props.data.currentShares[i].price);
+        setSelectedNumberShares(props.data.currentShares[i].number_shares);
+        break;
+      }
+    }
+  }, [currentShareID])
+
+  const rows = [];
+
+  for (let i = 0; i < props.data.currentShares.length; i++) {
+    if (props.data.currentShares[i].portfolio_id === Number(portfolioID)) {
+      let sharesLeft = props.data.currentShares[i].number_shares;
+      for (let j = 0; j < props.data.soldShares.length; j++) {
+        if (props.data.currentShares[i]._id === props.data.soldShares[j].share_id) {
+          sharesLeft -= props.data.soldShares[j].number_shares;
+        }
+      }
+      if (sharesLeft > 0) {
+        rows.push(<tr key={`row${i}`}>
+          <td>
+            <input
+              type='radio'
+              name='stockChoice'
+              value={props.data.currentShares[i]._id}
+              onChange={onInput}
+            />
+          </td>
+          <td>{props.data.currentShares[i].ticker_name}</td>
+          <td>{props.data.currentShares[i].date_purchased}</td>
+          <td>${props.data.currentShares[i].price}</td>
+          <td>{sharesLeft}</td>
+        </tr>)
+      }
+    }
+  }
+
+  useEffect(() => {
+    for (let i = 0; i < props.data.currentShares.length; i++) {
+      if (props.data.currentShares[i].portfolio_id === Number(portfolioID)) {
+        setPortName(props.data.currentShares[i].name);
+        break;
+      }
+    }
+  }, [portfolioID])
+
+
+
   return (
     <div>
-      <h3>Holdings of {portID}</h3>
+      <h3>Holdings of {portName}</h3>
       <div>
         <div className="tableContainer">
           <Table striped bordered hover variant="dark">
@@ -128,16 +227,7 @@ function PortfolioManager(props) {
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td>
-                  <input type="checkbox" id="checkBox"></input>
-                  </td>
-                <td>AAPL</td>
-                <td>9/29/20</td>
-                <td>$63.51</td>
-                <td>7</td>
-                <td>tech</td>
-              </tr>
+              {rows}
             </tbody>
           </Table>
         </div>
@@ -227,15 +317,15 @@ function PortfolioManager(props) {
         <Form>
           <Form.Group>
             <Form.Label>Ticker Name:</Form.Label>
-            <Form.Control type="text" placeholder="TSLA" />
+            <Form.Control type="text" placeholder={selectedTicker} />
             <Form.Label>Date Purchased:</Form.Label>
-            <Form.Control type="date" placeholder="29-09-2020" />
+            <Form.Control type="date" placeholder={selectedDate} />
             <Form.Label>Price Purchased:</Form.Label>
-            <Form.Control type="number" min="1" placeholder="420"/>
+            <Form.Control type="number" min="1" placeholder={selectedPrice}/>
             <Form.Label># of Shares:</Form.Label>
-            <Form.Control type="number" min="1" placeholder="1"/>
+            <Form.Control type="number" min="1" placeholder={selectedNumberShares}/>
             <Form.Label>Tags:</Form.Label>
-            <Form.Control type="text" placeholder="tech"/>
+            <Form.Control type="text" placeholder={selectedTags}/>
           </Form.Group>
         </Form>
         <Modal.Footer>
@@ -248,34 +338,69 @@ function PortfolioManager(props) {
   );
 };
 
-const PortfolioHistory = () => {
+const PortfolioHistory = (props) => {
   let { portfolioID } = useParams();
   const [portID, setPortID] = useState(portfolioID);
+  const [portName, setPortName] = useState('');
+  const [currentShareID, setCurrentShareID] = useState(null);
+
+  const onInput = e => {
+    setCurrentShareID(e.target.value);
+  }
+
+  const rows = [];
+
+  for (let i = 0; i < props.data.currentShares.length; i++) {
+    if (props.data.currentShares[i].portfolio_id === Number(portfolioID)) {
+      rows.push(<tr key={`row${i}`}>
+        <td>{props.data.currentShares[i].ticker_name}</td>
+        <td>{props.data.currentShares[i].date_purchased}</td>
+        <td>${props.data.currentShares[i].price}</td>
+        <td>{props.data.currentShares[i].number_shares}</td>
+      </tr>)
+    }
+  }
+  for (let i = 0; i < props.data.soldShares.length; i++) {
+    if (props.data.soldShares[i].portfolio_id === Number(portfolioID)) {
+      rows.push(<tr key={`row${i}`}>
+        <td>{props.data.soldShares[i].ticker_name}</td>
+        <td></td>
+        <td></td>
+        <td>{props.data.soldShares[i].number_shares}</td>
+        <td>{props.data.soldShares[i].date_sold}</td>
+        <td>${props.data.soldShares[i].sell_price}</td>
+        <td></td>
+      </tr>)
+    }
+  }
+
+  useEffect(() => {
+    for (let i = 0; i < props.data.currentShares.length; i++) {
+      if (props.data.currentShares[i].portfolio_id === Number(portfolioID)) {
+        setPortName(props.data.currentShares[i].name);
+        break;
+      }
+    }
+  }, [portfolioID])
 
   return(
     <div>
-      <h3>History of {portID}</h3>
+      <h3>History of {portName}</h3>
         <div className="tableContainer">
           <Table striped bordered hover variant="dark">
             <thead>
               <tr>
-                <th>Select</th>
                 <th>Ticker Name</th>
                 <th>Date Purchased</th>
                 <th>Purchase Price</th>
                 <th>Number of Shares</th>
+                <th>Date Sold</th>
+                <th>Sell Price</th>
                 <th>Tags</th>
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td></td>
-                <td>AAPL</td>
-                <td>9/29/20</td>
-                <td>$63.51</td>
-                <td>7</td>
-                <td>tech</td>
-              </tr>
+              {rows}
             </tbody>
           </Table>
         </div>
