dc680d4f17ef6e8d55eb7b60d019b412e8190c2e
Travis Frank <travis@travismfrank.com>
2020-10-13 16:38:09 -0400

    Added basic SuperTest suite

--- server/test/index.js
+++ server/test/index.js
@@ -1,5 +1,28 @@
 const test = require('tape');
+const request = require('supertest');
+const app = require('../server');
 
-test('First test!', function (t) {
-  t.end();
+test('Serves HTML', function (t) {
+  request(app)
+    .get('/')
+    .expect('Content-Type', 'text/html')
+    .expect(200)
+    .end(function (err, res) {
+      t.same(res.headers['content-type'], 'text/html; charset=UTF-8', 'Correct content-type');
+      t.same(res.text, '<!doctype html><html lang="en" dir="ltr"><head><meta charset="utf-8"><link rel="icon" href="./assets/favicon.ico" type="image/x-icon"><title>Smart Portfolio</title></head><body><div id="root"></div><script src="/bundle.js"></script></body></html>', 'Correct HTML text')
+      t.end();
+    });
+});
+
+test('Creates a Portfolio', function (t) {
+  request(app)
+    .post('/api/newPortfolio')
+    .set({name: 'Test Portfolio'})
+    .set('Cookie', ['user=frontendteam%40gmail.com'])
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .end(function (err, res) {
+      t.same(res.headers['content-type'], 'application/json; charset=utf-8', 'Correct content-type');
+      t.end();
+    });
 });
